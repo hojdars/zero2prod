b80350c7c5efa6edeb4c271a814045523b6866ec
Stepan Hojdar <hojdar.stepan@gmail.com>
2023-08-03 13:02:22 +0200

    Add basic logging to main and subscribe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use std::net::TcpListener;
 
 use sqlx::PgPool;
@@ -6,11 +7,17 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
+    tracing::info!("starting TCP listening at address={}", address);
     let listener = TcpListener::bind(address)?;
+
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
+
+    tracing::info!("starting the server");
     run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,14 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     connection_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    tracing::info!(
+        "id={} - adding '{}' '{}' as a new subscriber",
+        request_id,
+        form.name,
+        form.email
+    );
+
     match sqlx::query!(
         r#"
 INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -26,9 +34,12 @@ VALUES ($1, $2, $3, $4)
     .execute(connection_pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("id={} - new subscriber data saved", request_id);
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::info!("id={} - failed to execute query: {}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
