ea961b3dd4311d04898ed58115e33ceb05c8befe
Stepan Hojdar <hojdar.stepan@gmail.com>
2023-08-04 23:07:36 +0200

    Utilize tracing's spans to improve correlation of log messages

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
-use env_logger::Env;
 use std::net::TcpListener;
 
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     tracing::info!("starting TCP listening at address={}", address);
     let listener = TcpListener::bind(address)?;
 
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
 
     tracing::info!("starting the server");
     run(listener, connection_pool)?.await
